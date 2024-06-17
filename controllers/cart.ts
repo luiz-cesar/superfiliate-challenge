705382416e42ba56dabfa1d408802b45983bab22
Victor Feijo <victor.feijoa@gmail.com>
2024-06-17 18:30:59 -0300

    add feebacks

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -1,4 +1,6 @@
 import * as zod from "zod";
+// PRO: Ramda is a really cool library, but could probably
+// use the reduce function from the array standard library.
 import { partition, reduce } from "ramda";
 import {
   RequestMethod,
@@ -6,6 +8,7 @@ import {
   buildEndpointRegister,
 } from "../common/requests";
 
+// PRO: Usage of typescript
 export type Item = {
   name: string;
   price: number;
@@ -30,12 +33,15 @@ const isDiscountableItem = (item: Item) =>
 export const calculateCartController = buildEndpointRegister({
   method: RequestMethod.GET,
   path: "/cart",
+  // PRO: Usage of validations!
   validator: zod.object({
     cart: zod.object({
       reference: zod.string(),
       lineItems: zod
         .object({
           name: zod.string(),
+          // CON: I would probably also validate for negative prices. If a malicious user
+          // sends a cart with negative value items, the final price could be zero or even negative.
           price: zod.coerce.number(),
           collection: zod.string(),
         })
@@ -45,6 +51,7 @@ export const calculateCartController = buildEndpointRegister({
   handler: (data) => {
     const items = data.cart.lineItems;
 
+    // PRO: Great strategy! breaking the problem into two "parts".
     const [withDiscount, withoutDiscount] = partition(
       isDiscountableItem,
       items
@@ -52,6 +59,9 @@ export const calculateCartController = buildEndpointRegister({
 
     const discount = getDiscountPercentage(withDiscount.length);
 
+    // This is more like a personal preference, but I think it would easier to read
+    // if you calculate the total by summing responseData.items -> finalPrice. Just
+    // to make it very explicit that the total is just a sum of all items in the cart.
     const total =
       sumPrices(withoutDiscount) + sumPrices(withDiscount) * discount;
 
@@ -66,6 +76,9 @@ export const calculateCartController = buildEndpointRegister({
           finalPrice: Math.round(item.price * discount * 1e2) / 1e2,
         })),
       ],
+      // CON: Improve the precision of the total value. On e-commerce we usually
+      // work with decimal values, but here the API return values like 12.2199999999999999
+      // instead of 12.22 or 12.21.
       total,
     };
 
